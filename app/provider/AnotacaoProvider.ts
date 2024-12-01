681831c52056c9f121806a89b0fc3fbdd3689ded
tarcizo12 <tarcizon12@gmail.com>
2024-12-01 13:44:09 -0300

    feat: Configura acao incial de postAnotacao

--- app/provider/AnotacaoProvider.ts
+++ app/provider/AnotacaoProvider.ts
@@ -12,6 +12,25 @@ export default class AnotacaoProvider {
     return await AnotacaoProvider.getRequest<AnotacaoPacienteModel[]>(url);
   }
 
+  static async salvarNovaAnotacao(idPaciente: number, anotacao: AnotacaoPacienteModel): Promise<number> {
+    const url = PATHS_API.ANOTACOES.SALVAR_ANOTACAO;
+    
+    const response = await AnotacaoProvider.postRequest<AnotacaoPacienteModel>(url, anotacao);
+
+    return response.idAnotacao;
+  }
+
+
+  private static async postRequest<T>(url: string, body: T): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await axios.post(url, body);
+      return response.data; 
+    } catch (error) {
+      console.error('Erro ao realizar POST:', error);
+      throw error;
+    }
+  }
+
   private static async getRequest<T>(url: string): Promise<T> {
     try {
       const response: AxiosResponse<T> = await axios.get(url);
@@ -22,16 +41,5 @@ export default class AnotacaoProvider {
     }
   }
   
-  private static async postRequest<T>(
-    url: string,
-    body: AnotacaoPacienteModel
-  ): Promise<T> {
-    try {
-      const response: AxiosResponse<T> = await axios.post(url, body);
-      return response.data;
-    } catch (error) {
-      console.error('Erro ao realizar POST:', error);
-      throw error;
-    }
-  }
+
 }
--- app/provider/PathsApi.ts
+++ app/provider/PathsApi.ts
@@ -3,7 +3,8 @@ const basePathAnotacoes = "anotacao/"
 
 const PATHS_API = {
     ANOTACOES : {
-        BUSCAR_ANOTACAO_BY_PACIENTE : `${localHostBasePath}${basePathAnotacoes}buscarAnotacaoPorIdPaciente?idPaciente=:id`
+        BUSCAR_ANOTACAO_BY_PACIENTE : `${localHostBasePath}${basePathAnotacoes}buscarAnotacaoPorIdPaciente?idPaciente=:id`,
+        SALVAR_ANOTACAO : `${localHostBasePath}${basePathAnotacoes}registrarAnotacao`
     }
 } 
 
