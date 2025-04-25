d21a26a8e4d511c5b32abaeeae177f78bcb061ce
Bruno Neves <bruno.pneves9@gmail.com>
2025-04-25 19:12:06 -0300

    Criando o componente para realizar a geração do titulo por IA

--- app/screens/featurePaciente/homeScreen/HomePacienteScreen.tsx
+++ app/screens/featurePaciente/homeScreen/HomePacienteScreen.tsx
@@ -1,24 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import {
-  View,
-  ScrollView,
-  TouchableOpacity,
-  Image,
-  Text,
-  StyleSheet,
-  TextInput,
-} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, TextInput, Dimensions } from 'react-native';
 import { AnotacaoPacienteModel } from '@/constants/models/AnotacaoPacienteModel';
 import { AnotacaoPacienteMockFactory } from '@/constants/mock/AnotacaoPacienteMockFactory';
 import AddButton from '@/common/AddButton';
 import AnotacaoModal from './components/AnotacaoModal';
 import RenderCellsAnotacoes from './components/RenderCellsAnotacoes';
 import AnotacaoProvider from '@/app/provider/AnotacaoProvider';
-import Icon1 from '@/icons/calendar.png';
-import Icon2 from '@/icons/notes.png';
-import Icon3 from '@/icons/add.png';
-import Icon4 from '@/icons/person.png';
-import BackButton from '@/common/BackButton';
 import Sidemenu from '../../Sidemenu';
 
 const ID_PACIENTE_MOCK = 1;
@@ -29,8 +16,8 @@ export default function HomePacienteScreen() {
   const [modalVisible, setModalVisible] = useState(false);
   const [listaAnotacoesAtual, setListaAnotacoesAtual] = useState<AnotacaoPacienteModel[]>([]);
   const [descricaoAnotacaoText, setDescricaoAnotacaoText] = useState('');
+  const [tituloAnotacaoText, setTituloAnotacaoText] = useState('');
   const [emojiSelecionado, setEmojiSelecionado] = useState<string | null>(null);
-
 
   const atualizaListaAnotacoes = (): void => {
     AnotacaoProvider.obterListaAnotacoesPaciente(ID_PACIENTE_MOCK).then(
@@ -47,112 +34,137 @@ export default function HomePacienteScreen() {
   }, [modalVisible]);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.headerContainer}>
+    <View style={styles.background}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Nova anotação</Text>
+      </View>
 
-          <Text style={styles.headerTitle}>Nova anotação</Text>
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            padding: 16,
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <View style={styles.inputContainer}>
-            <Text style={{ fontSize: 16, marginLeft: 16, marginBottom: 8 }}>
-              Como você está se sentindo?
-            </Text>
-
-            <View>
-              <TextInput
-                style={styles.textInput}
-                placeholder="Este espaço é seu: escreva sobre algo que viveu e como isso te fez sentir..."
-                multiline
-                value={descricaoAnotacaoText}
-                onChangeText={setDescricaoAnotacaoText}
-                textAlignVertical="top"
-              />
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.addButtonContainer}>
-          <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.addButton}>
-            <Text style={styles.addButtonText}>Salvar</Text>
-          </TouchableOpacity>
-        </View>
-
-        <RenderCellsAnotacoes anotacoes={listaAnotacoesAtual} />
-        <AnotacaoModal
-          idPaciente={ID_PACIENTE_MOCK}
-          visible={modalVisible}
-          setVisibleFalseModal={() => setModalVisible(false)}
+      <View style={styles.inputContainer}>
+        <Text style={{ fontSize: 16, marginLeft: 16, marginBottom: 8 }}>
+          Como você está se sentindo?
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Este espaço é seu: escreva sobre algo que viveu e como isso te fez sentir..."
+          multiline
+          scrollEnabled
+          value={descricaoAnotacaoText}
+          onChangeText={setDescricaoAnotacaoText}
+          textAlignVertical="top"
         />
-      </ScrollView>
+      </View>
+
+      <View style={styles.titleContainer}>
+        <View style={styles.textTitleContainer}>
+          <Text>Titulo</Text>
+        </View>
+        <View style={{ alignItems: 'center', justifyContent: 'center', width: '100%', borderBlockColor: 'black', borderRadius: }}>
+          <TextInput
+            style={styles.titleTextInput}
+            placeholder="Digite o título da anotação"
+            value={tituloAnotacaoText}
+            onChangeText={setTituloAnotacaoText}
+            scrollEnabled
+            multiline={false}
+            textAlignVertical="center"
+            textAlign="left"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: 16}}>
+            Gerar titulo automaticamente
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.addButton}>
+          <Text style={styles.addButtonText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <RenderCellsAnotacoes anotacoes={listaAnotacoesAtual} />
+
+      <AnotacaoModal
+        idPaciente={ID_PACIENTE_MOCK}
+        visible={modalVisible}
+        setVisibleFalseModal={() => setModalVisible(false)}
+      />
 
       <Sidemenu />
     </View>
   );
 }
 
+const widthScreen = Dimensions.get('window').width;
+const heightScreen = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
-  scrollContainer: {
-    width: '100%',
-    height: '100%',
+  background: {
+    backgroundColor: '#ffffff',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: widthScreen,
+    flex: 1,
   },
   headerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 16,
     backgroundColor: '#20A69F',
-    width: '100%',
-    height: 60,
-    marginBottom: 150,
-  },
-  backButton: {
-    backgroundColor: '#a0ff7d',
-    borderRadius: 25,
-    width: '10%',
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 10,
-  },
-  backButtonText: {
-    color: 'black',
-    fontSize: 20,
-    fontWeight: 'bold',
+    width: widthScreen,
+    height: heightScreen * 0.07,
   },
   headerTitle: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
   },
   inputContainer: {
-    width: '90%',
-    height: 400,
+    width: widthScreen * 0.9,
+    height: heightScreen * 0.3,
     borderWidth: 1,
     borderColor: '#CCCCCC',
     borderRadius: 10,
     padding: 10,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  textInput: {
+    width: '100%',
+    height: '80%',
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 14,
+    backgroundColor: '#FFFFFF',
+    textAlignVertical: 'top',
+  },
+  titleContainer: {
+    backgroundColor: '#ffffff',
+    width: widthScreen * 0.9,
+    height: heightScreen * 0.18,
+    marginTop: 20,
+    padding: 10,
+  },
+  textTitleContainer: {
+    width: '100%',
+    height: '20%',
+    backgroundColor: 'red',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  titleTextInput: {
+    width: '100%',
+    height: '60%',
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 14,
+    backgroundColor: '#FFFFFF',
   },
   addButtonContainer: {
     alignItems: 'center',
@@ -161,43 +173,15 @@ const styles = StyleSheet.create({
   },
   addButton: {
     backgroundColor: '#20A69F',
-    width: '40%',
-    height: 45,
+    width: widthScreen * 0.25,
+    height: heightScreen * 0.05,
     borderRadius: 30,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 150,
   },
   addButtonText: {
     color: 'white',
     fontSize: 15,
     fontWeight: 'bold',
-  },
-  bottomMenu: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    paddingVertical: 15,
-    backgroundColor: '#fff',
-    borderTopWidth: 1,
-    borderTopColor: '#20A69F',
-    width: '100%',
-    height: '7%',
-  },
-  menuOption: {
-    padding: 10,
-    alignItems: 'center',
-  },
-  menuImage: {
-    width: 30,
-    height: 30,
-    resizeMode: 'contain',
-  },
-  textInput: {
-    width: 300,
-    height: 150,
-    padding: 10,
-    borderRadius: 8,
-    fontSize: 14,
   },
 });
